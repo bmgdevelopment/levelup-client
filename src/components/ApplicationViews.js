5173d589888507c51198c71eeca1e9d2fed40236
Brittany Garrett <brittanymgarrett@gmail.com>
2021-12-21 23:38:36 -0600

    Updated with games

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,24 @@
-import React from "react"
-import { Route } from "react-router-dom"
+import React from "react";
+import { Route } from "react-router-dom";
+import { GameList } from "./game/GameList.js";
+import { GameProvider } from "./game/GameProvider.js";
 
 export const ApplicationViews = () => {
-    return <>
-        <main style={{
-            margin: "5rem 2rem",
-            backgroundColor: "lightgoldenrodyellow"
-        }}>
-            Application views
-        </main>
+  return (
+    <>
+      <main
+        style={{
+          margin: "5rem 2rem",
+          lineHeight: "1.75rem",
+          backgroundColor: "lightgreen"
+        }}
+      >
+        <GameProvider>
+          <Route exact path="/">
+            <GameList />
+          </Route>
+        </GameProvider>
+      </main>
     </>
-}
+  );
+};
